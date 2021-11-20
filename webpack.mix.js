9fab6d2ca334eea1d773e4bc4e72417a317f5949
武田 憲太郎 <takeda@youmind.jp>
2021-11-20 17:52:00 +0900

    sourcemapの出力

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/css/app.scss', 'public/css', [
         //
-    ]);
+    ])
+    .sourceMaps(false, 'source-map');
